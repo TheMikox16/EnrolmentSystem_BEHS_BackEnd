f977e7b1bc41d775286f5cbf3c2e2e0d15a9a319
Kemuce <kevin.murillo@ucrso.info>
2021-11-05 20:47:35 -0600

    LogicaFechasKevin
    Tiene la logica para la fecha de las paginas de inicio, de sesion y de registro.

---
+++ Datos/CapaDatos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnrolmentSystem_BEHS.Datos
+{
+    public class CapaDatos
+    {
+
+        private EnrollEntities entity = new EnrollEntities();
+
+        public CapaDatos()
+        {
+
+        }
+
+        public int TitleDate()
+        {
+            using (entity = new EnrollEntities())
+            {
+                int anno = entity.FechaAnno();
+                return anno;
+            }
+        }
+
+
+
+
+    }
+}
