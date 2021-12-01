68c273d8b6c69d005e3215be581cc8eecc9aff18
Kemuce <kevin.murillo@ucrso.info>
2021-11-30 19:34:12 -0600

    Push cambios logicaGeneral, capaDatos y base de datos
    logicaGeneral.cs: comentarios de varios metodos que dan un resumen de uso de cada metodo. Al metodo registrar se le ha implementado la llamada al metodo verificarcorreo, y a esta última se le ha mejorado la lógica de validación del correo a registrar para que no acepte signos despues del @, por ejemplo al colocar hola@.com, igualmente se valida el telefono con la creacion de un nuevo metodo: verificartelefono. Creacion de un metodo para actualizar los datos del usuario.
    -capadatos.cs: solo implementar los nuevos metodos y llamadas según correspondan a los metodos nuevos e implementados (Llenarinformacion, actualizarusuario).
    -Base de datos: creación de los stored porcedures necesarios.

--- Logica/LogicaGeneral.cs
+++ Logica/LogicaGeneral.cs
@@ -16,14 +16,47 @@ namespace EnrolmentSystem_BEHS.Logica
             return temp;
         }
 
+        /**
+         * Metodo para ingresar al sistema. Es una llamada a la capa de datos 
+         * 
+        */
         public List<Usuarios> Ingresar (string c, string p)
         {
             return datos.Ingresar(c, p);
         }
 
+<<<<<<< Updated upstream
         public string Registrar(string c, string p)
         {
             bool temp = datos.Registrar(c, p);
+=======
+        /**
+         * Metodo para registrar un nuevo usuario. Verifica que las contraseñas sean correctas y cumplan
+         * con ciertos criterios de seguridad. Asi mismo, verifica los valores numericos y si el correo
+         * digitado esta o no registrado.
+        */
+        public string Registrar(string c, string p, string co, string u, string pho)
+        {
+            
+            if (!VerificarCorreo(c))
+            {
+                return "Email sintaxis is not allowed, please write a valid email address";
+            }
+
+            if (!p.Equals(co))
+            {
+                return "Passwords doesn't match, try again";
+            }
+
+            if (!VerificarTelefono(pho))
+            {
+                return "Phone Number has a invalid value, please try with a different value";
+            }
+
+            long phoneNumb = Int64.Parse(pho);
+
+            bool temp = datos.Registrar(c, p, u, phoneNumb);
+>>>>>>> Stashed changes
 
             if (temp == true)
             {
@@ -33,29 +66,57 @@ namespace EnrolmentSystem_BEHS.Logica
             return "That e-mail is already registered!";
         }
 
+        /**
+         * Metodo auxiliar que valida un correo según ciertos parametros
+         * 
+         */
         public bool VerificarCorreo(string s)
         {
             foreach(char c in s)
             {
                 if (c.Equals('@') && s.ToCharArray()[s.Length-1] != c)
                 {
-                    return true;
+                    if (Char.IsLetter(s[s.IndexOf('@') + 1]))
+                    {
+                        return true;
+                    }
                 }
             }
             return false;
         }
 
+        /**
+         * Metodo que intenta parsear el valor numerico del telefono. Si falla, regresa un false, si tiene exito, regresa true
+         */ 
+        public bool VerificarTelefono(string s)
+        {
+            bool temp = Int64.TryParse(s,out long result);
+            return temp;
+        }
 
+        /**
+         * Metodo que realiza la matricula por medio de una llamada a la base de datos. Se utiliza todos los datos introducidos
+         * en el formulario de Matricular.aspx
+         */
         public bool Matricular(string correo, string sN, string sL1, string sL2, long id, byte idT, string sNE, string sLE1, string sLE2, long idE, byte idTE, string procedence, int gen, int genE, int grado, int telf)
         {
             return datos.Matricular(correo, sN, sL1, sL2, id, idT, sNE, sLE1, sLE2, idE, idTE, procedence, gen, genE, grado, telf);
         }
 
+
+        /**
+         * Metodo que determina si un estudiante ya se ha matriculado en ese año o si necesitara matricularse nuevamente 
+         * ya que es un nuevo año 
+         */
         public string EnrolledCurrentYear(string c)
         {
             return datos.EnrolledCurrentYear(c);
         }
 
+        /**
+         * Metodo encargado de llenar los datos enviados al proceso de matricula. Consulta a la base de datos por
+         * medio de la capa de datos y contruye el string a ser desplegado por medio de la consulta.
+         */
         public string LlenarDatos(string c)
         {
             string temp = "";
@@ -73,6 +134,44 @@ namespace EnrolmentSystem_BEHS.Logica
                 "Enrollment Status: " + list[0].estado;
 
             return temp;
+        }
+
+        /**
+         * Metodo encargado de conectarse con la capa de datos para realizar la correcta actualización de los datos del usuario
+         */
+        public string ActualizarUsuario(string c, string n, string pho)
+        {
+            if (!VerificarTelefono(pho))
+            {
+                return "Phone Number has a invalid value, please try with a different value";
+            }
+
+            long phoneNumb = Int64.Parse(pho);
+
+            bool temp = datos.ActualizarUsuario(c, n, phoneNumb);
+
+            if (temp)
+            {
+                return "User information updated successfully";
+            }
+            else
+            {
+                return "Fail updating user information!!";
+            }
+
+        }
+
+        public string[] LlenarInformacion(string c)
+        {
+            Usuarios u = datos.LlenarInformacion(c);
+
+            string[] temp = new string[8];
+            temp[0] = u.correo.ToString();
+            temp[1] = u.nombre.ToString();
+            temp[2] = u.telefono.ToString();
+
+            return temp;
+
         }
 
     }
